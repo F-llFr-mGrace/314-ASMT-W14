874785069beb426103bb24786fb66da274b9458f
Grace Fowler <f.llfr.mgrace@gmail.com>
2025-04-22 20:35:21 -0400

    Unity init

---
+++ 314-ASMT-W14-Unity/Assets/Scripts/System/ProgressControl.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
+using System;
+
+public class ProgressControl : MonoBehaviour
+{
+    public UnityEvent<string> OnStartGame;
+    public UnityEvent<string> OnChallengeComplete;
+
+    [Header("Start Button")]
+    [SerializeField] XrButtonInteractable startButton;
+    [SerializeField] GameObject keyIndicatorLight;
+
+    [Header("Drawer Interactable")]
+    [SerializeField] DrawerInteractable drawer;
+    XRSocketInteractor drawerSocket;
+
+    [Header("Combo Lock")]
+    [SerializeField] CombinationLock comboLock;
+
+    [Header("The Wall")]
+    [SerializeField] TheWall wall;
+    XRSocketInteractor wallSocket;
+    [SerializeField] GameObject teleportationAreas;
+        
+    [Header("Library")]
+    [SerializeField] SimpleSliderControl librarySlider;
+
+    [Header("Challenge Settings")]
+    [SerializeField] string startGameString;
+    [SerializeField] string[] challengeStrings;
+    private bool startGameBool;
+    private int challengeNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startButton != null)
+        {
+            startButton.selectEntered.AddListener(StartButtonPressed);
+        }
+        OnStartGame?.Invoke(startGameString);
+        SetDrawerInteractable();
+        if (comboLock != null)
+        {
+            comboLock.UnlockAction += OnComboUnlocked;
+        }
+        if (wall != null)
+        {
+            SetWall();
+        }
+        if(librarySlider != null)
+        {
+            librarySlider.OnSliderActive.AddListener(LibrarySliderActive);
+        }
+    }
+
+    private void LibrarySliderActive()
+    {
+        ChallengeComplete();
+    }
+
+    private void ChallengeComplete()
+    {
+        challengeNumber++;
+        if (challengeNumber < challengeStrings.Length)
+        {
+            OnChallengeComplete?.Invoke(challengeStrings[challengeNumber]);
+        }
+        else if (challengeNumber >= challengeStrings.Length)
+        {
+            //****ALL CHALLENGES COMPLETE
+        }
+    }
+    private void StartButtonPressed(SelectEnterEventArgs arg0)
+    {
+        if (!startGameBool)
+        {
+            startGameBool = true;
+            if (keyIndicatorLight != null)
+            {
+                keyIndicatorLight.SetActive(true);
+            }
+            if (challengeNumber < challengeStrings.Length)
+            {
+                OnStartGame?.Invoke(challengeStrings[challengeNumber]);
+            }
+        }
+    }
+    private void SetDrawerInteractable()
+    {
+        if (drawer != null)
+        {
+            drawer.OnDrawerDetach.AddListener(OnDrawerDetach);
+            drawerSocket = drawer.GetKeySocket;
+            if (drawerSocket != null)
+            {
+                drawerSocket.selectEntered.AddListener(OnDrawerSocketed);
+            }
+        }
+    }
+
+    private void OnDrawerDetach()
+    {
+        ChallengeComplete();
+    }
+
+    private void OnDrawerSocketed(SelectEnterEventArgs arg0)
+    {
+        ChallengeComplete();
+    }
+    private void OnComboUnlocked()
+    {
+        ChallengeComplete();
+    }
+    private void SetWall()
+    {
+        wall.OnDestroy.AddListener(OnDestroyWall);
+        wallSocket = wall.GetWallSocket;
+        if (wallSocket != null)
+        {
+            wallSocket.selectEntered.AddListener(OnWallSocketed);
+        }
+    }
+
+    private void OnWallSocketed(SelectEnterEventArgs arg0)
+    {
+        ChallengeComplete();
+    }
+
+    private void OnDestroyWall()
+    {
+        ChallengeComplete();
+        if(teleportationAreas != null)
+        {
+            teleportationAreas.SetActive(true);
+        }
+    }
+}
